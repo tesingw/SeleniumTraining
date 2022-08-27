a206e869deffe3bb27e3e5cafd614a08c94ecff9
unknown <tesingw>
2022-08-27 10:11:17 +0530

    Discussed on Selenium Waits Commands

--- SeleniumTests/src/test/java/TestNGTests.java
+++ SeleniumTests/src/test/java/TestNGTests.java
@@ -4,22 +4,32 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class TestNGTests extends BaseTest {
 
     @Test(priority = 1)
     public void test1() throws IOException, InterruptedException {
         driver.get("https://www.flipkart.com/");
+        WebElement ele = driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']"));
+        ele.click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         WebElement ele1 = driver.findElement(By.xpath("//img[@alt='Electronics']"));
         Actions action = new Actions(driver);
         action.moveToElement(ele1).build().perform();
-        Thread.sleep(3000);
+        //Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         WebElement ele2 = driver.findElement(By.xpath("//a[contains(text(),'Audio')]"));
-        takesScreenshot();
+        //WebDriverWait wait = new WebDriverWait(driver,30);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Audio')]")));
+        //wait.pollingEvery(6);
+        //takesScreenshot();
         ele2.click();
     }
 
@@ -55,6 +65,6 @@ public class TestNGTests extends BaseTest {
 
     public void takesScreenshot() throws IOException {
         File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(file, new File(".\\Screenshots" + ".png"));
+        FileHandler.copy(file, new File(".\\Screenshots" + "\\" + "Screenshots" + ".png"));
     }
 }
--- SeleniumTests/src/test/java/TrainingTests.java
+++ SeleniumTests/src/test/java/TrainingTests.java
@@ -8,27 +8,27 @@ import java.util.Set;
 
 public class TrainingTests extends BaseTest {
 
-//    @Test
-//    public void handlingMultipleWindows() {
-//        driver.get("https://demoqa.com/browser-windows");
-//        WebElement ele1 = driver.findElement(By.id("windowButton"));
-//        String parentWindow = driver.getWindowHandle();
-//        ele1.click();
-//        Set<String> getAllWindows = driver.getWindowHandles();
-//        for (String window : getAllWindows) {
-//            if (!window.equalsIgnoreCase(parentWindow)) {
-//                driver.switchTo().window(window);
-//                driver.manage().window().maximize();
-//                WebElement ele2 = driver.findElement(By.id("sampleHeading"));
-//                String getValue = ele2.getText();
-//                System.out.println(getValue);
-//                driver.close();
-//            }
-//        }
-//        driver.switchTo().window(parentWindow);
-//        WebElement ele3 = driver.findElement(By.xpath("//div[contains(text(),'Browser Windows')]"));
-//        System.out.println(ele3.getText());
-//    }
+    @Test
+    public void handlingMultipleWindows() {
+        driver.get("https://demoqa.com/browser-windows");
+        WebElement ele1 = driver.findElement(By.id("windowButton"));
+        String parentWindow = driver.getWindowHandle();
+        ele1.click();
+        Set<String> getAllWindows = driver.getWindowHandles();
+        for (String window : getAllWindows) {
+            if (!window.equalsIgnoreCase(parentWindow)) {
+                driver.switchTo().window(window);
+                driver.manage().window().maximize();
+                WebElement ele2 = driver.findElement(By.id("sampleHeading"));
+                String getValue = ele2.getText();
+                System.out.println(getValue);
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
+        WebElement ele3 = driver.findElement(By.xpath("//div[contains(text(),'Browser Windows')]"));
+        System.out.println(ele3.getText());
+    }
 
     //boolean : true or false
 
